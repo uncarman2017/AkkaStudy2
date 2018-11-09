0167467c5c07d2dedf485bd2153a919401b9c9cb
yujiawei <yujiawei@ciicsh.com>
2018-11-09 16:58:19 +0800

    check in practise 1 again

--- akka.practise1/src/main/java/org/redmaplesoft/akka/practise1/App4.java
+++ akka.practise1/src/main/java/org/redmaplesoft/akka/practise1/App4.java
@@ -1,8 +1,15 @@
 package org.redmaplesoft.akka.practise1;
 
+import akka.actor.ActorNotFound;
 import akka.actor.ActorRef;
+import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.dispatch.OnFailure;
+import akka.dispatch.OnSuccess;
+import akka.util.Timeout;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
 /**
  * @author Max Yu
@@ -15,5 +22,24 @@ public class App4 {
         ActorRef actorRef = system.actorOf(Props.create(LookupActor.class), "lookupActor");
         actorRef.tell("find", actorRef);
 
+        // 第二种方式查找Actor
+        ActorSelection as = system.actorSelection("/user/lookupActor/targetActor");
+        Timeout timeout = new Timeout(Duration.create(2,"seconds"));
+        Future<ActorRef> fu = as.resolveOne(timeout);
+        fu.onSuccess(new OnSuccess<ActorRef>() {
+            @Override
+            public void onSuccess(ActorRef result) throws Throwable {
+                System.out.println("查找到Actor:" + result);
+            }
+        },system.dispatcher());
+
+        fu.onFailure(new OnFailure() {
+            @Override
+            public void onFailure(Throwable failure) throws Throwable {
+                if(failure instanceof ActorNotFound){
+                    System.out.println("没有找到Actor: " + failure.getMessage());
+                }
+            }
+        },system.dispatcher());
     }
 }
